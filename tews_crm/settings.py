678b82a659039b91a5e2d8dddb29f8d2c34d7758
Yavnesh Sharma <yavnesh.sharma@gmail.com>
2024-05-03 01:34:07 +0530

    add host for leapcell

--- tews_crm/settings.py
+++ tews_crm/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-k@#pib9$j)va($^d643wcpn2-#!in&1i!s0p8kzpy&b9^mxklb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'now.sh', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'now.sh', 'localhost', 'leapcell.io']
 
 
 # Application definition
